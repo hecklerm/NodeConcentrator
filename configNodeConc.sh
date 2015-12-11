2463cc6a50cda249bbadcaa5f0093c7b0ea48b81
Mark Heckler <mark.heckler@gmail.com>
2015-12-11 17:05:22 -0600

    Updated primary script.

--- configNodeConc.sh
+++ configNodeConc.sh
@@ -6,14 +6,16 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install oracle-java8-jdk
 
-sudo apt-get install libjpeg8-dev
-sudo apt-get install imagemagick
-tar xvzf mjpg-streamer*.tar.gz
-cd mjpg-streamer*
-make
-cp mjpg_streamer /usr/local/bin
-cp input_*.so /usr/local/bin
-cp output_*.so /usr/local/bin
+sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
+sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+wget http://sourceforge.net/code-snapshots/svn/m/mj/mjpg-streamer/code/mjpg-streamer-code-182.zip
+unzip mjpg-streamer-code-182.zip
+cd mjpg-streamer-code-182/mjpg-streamer
+make mjpg_streamer input_file.so output_http.so
+sudo cp mjpg_streamer /usr/local/bin
+sudo cp output_http.so input_file.so /usr/local/lib/
+sudo cp -R www /usr/local/www
+mkdir /tmp/stream
 
 cp runCam.sh ~
 mkdir ~/PiRemote
