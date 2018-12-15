7a39e4c6e852397677d80adb9f26e3b861fe2851
Mark Heckler <mark.heckler@gmail.com>
2018-12-15 12:57:34 -0600

    Updated to JDK11 (OpenJDK for ARM) & included archive of same

--- configNodeConc.sh
+++ configNodeConc.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+JDK_ARCHIVE=OpenJDK11U-jdk_aarch64_linux_hotspot_11.0.1_13.tar.gz
+
 # In case we're still booting into the GUI
 sudo systemctl set-default multi-user.target
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install oracle-java8-jdk
+# sudo apt-get install oracle-java8-jdk
 
 sudo apt-get install mosquitto mosquitto-clients
 
 sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
 sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+
+tar -zxvf $JDK_ARCHIVE -C /var/opt/java
 
 cp runCam.sh ~
 cp stopCam.sh ~
@@ -36,4 +40,5 @@ echo   memory split - optional
 echo   overclock - High for Pi, Pi2 for Pi2
 echo   enable ssh
 echo   enable camera - if used in remote config
+echo   add env var to .bashrc for JAVA_HOME (/var/opt/java/???)
 
