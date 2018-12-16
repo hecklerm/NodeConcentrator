6e106e5e518f81618cc304058c016eaf45ee3c39
Mark Heckler <mark.heckler@gmail.com>
2018-12-16 00:01:24 -0600

    Revert to Oracle JDK8 for now bc only ARM builds available are 64-bit

--- configNodeConc.sh
+++ configNodeConc.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
-JDK_ARCHIVE=OpenJDK11U-jdk_aarch64_linux_hotspot_11.0.1_13.tar.gz
+# JDK_ARCHIVE=OpenJDK11U-jdk_aarch64_linux_hotspot_11.0.1_13.tar.gz
+# JAVA_HOME=/var/opt/java/jdk-11.0.1+13
 
 # In case we're still booting into the GUI
 sudo systemctl set-default multi-user.target
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-# sudo apt-get install oracle-java8-jdk
+sudo apt-get install oracle-java8-jdk
 
 sudo apt-get install mosquitto mosquitto-clients
 
 sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
 sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 
-sudo mkdir /var/opt/java
-sudo tar -zxvf $JDK_ARCHIVE -C /var/opt/java
+# The only ARM builds available are 64 bit; revert to Oracle JDK8 32 bit until I can create my own build :(
+# sudo mkdir /var/opt/java
+# sudo tar -zxvf $JDK_ARCHIVE -C /var/opt/java
+# sudo update-alternatives --install /usr/bin/java java $JAVA_HOME/bin/java 1000
+# sudo update-alternatives --install /usr/bin/javac javac $JAVA_HOME/bin/javac 1000
+# sudo update-alternatives --install /usr/bin/javadoc javadoc $JAVA_HOME/bin/javadoc 1000
+# sudo update-alternatives --install /usr/bin/javah javah $JAVA_HOME/bin/javah 1000
+# sudo update-alternatives --install /usr/bin/javap javap $JAVA_HOME/bin/javap 1000
 
 cp runCam.sh ~
 cp stopCam.sh ~
@@ -41,5 +48,5 @@ echo   memory split - optional
 echo   overclock - High for Pi, Pi2 for Pi2
 echo   enable ssh
 echo   enable camera - if used in remote config
-echo   add env var to .bashrc for JAVA_HOME - /var/opt/java/???
+# echo   add env var to .bashrc for JAVA_HOME - /var/opt/java/???
 
