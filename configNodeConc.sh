6936dde4417be90eeabc568f369f1014a175441f
Mark Heckler <mark.heckler@gmail.com>
2018-12-15 23:15:04 -0600

    Updated to fix JDK extraction issues & typo

--- configNodeConc.sh
+++ configNodeConc.sh
@@ -4,8 +4,8 @@ JDK_ARCHIVE=OpenJDK11U-jdk_aarch64_linux_hotspot_11.0.1_13.tar.gz
 # In case we're still booting into the GUI
 sudo systemctl set-default multi-user.target
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 # sudo apt-get install oracle-java8-jdk
 
 sudo apt-get install mosquitto mosquitto-clients
@@ -13,7 +13,8 @@ sudo apt-get install mosquitto mosquitto-clients
 sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
 sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 
-tar -zxvf $JDK_ARCHIVE -C /var/opt/java
+sudo mkdir /var/opt/java
+sudo tar -zxvf $JDK_ARCHIVE -C /var/opt/java
 
 cp runCam.sh ~
 cp stopCam.sh ~
@@ -40,5 +41,5 @@ echo   memory split - optional
 echo   overclock - High for Pi, Pi2 for Pi2
 echo   enable ssh
 echo   enable camera - if used in remote config
-echo   add env var to .bashrc for JAVA_HOME (/var/opt/java/???)
+echo   add env var to .bashrc for JAVA_HOME - /var/opt/java/???
 
